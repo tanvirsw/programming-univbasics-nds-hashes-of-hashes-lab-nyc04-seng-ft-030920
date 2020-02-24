5ae10089874860cbbc17d9678b078bc87a5e7966
Tanvir Walia <tarywalia@gmail.com>
2020-02-24 17:40:34 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,24 +13,47 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
 # }
 
 def naming_system
+
+  {
+    label: "Kingdom",
+    sub_category:{
+      label: "Phylum",
+      sub_category:{
+        label: "Class",
+        sub_category:{
+          label:"Order",
+          sub_category:{
+            label:"Family",
+            sub_category:{
+              label:"Genus",
+              sub_category:{
+                label:"Species",
+                sub_category:nil 
+              }
+            }
+          }
+        }
+      }
+    }
+  }
   # Remember:
   #  Kingdom
   #  Phylum
