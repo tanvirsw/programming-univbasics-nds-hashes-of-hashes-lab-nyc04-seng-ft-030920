a2251488453155a4982a2fd5e6921745a77e62ed
Tanvir Walia <tarywalia@gmail.com>
2020-02-24 17:40:21 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,6 +44,10 @@ def naming_system
             label:"Family",
             sub_category:{
               label:"Genus",
+              sub_category:{
+                label:"Species",
+                sub_category:nil 
+              }
             }
           }
         }
